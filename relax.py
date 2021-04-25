3fa2f5c08963f6526b96611252f93a81017ffad0
KAK_ZED <71234040+968hacker@users.noreply.github.com>
2021-04-25 04:04:09 +0300

    Update relax.py

--- relax.py
+++ relax.py
@@ -3,7 +3,7 @@ from time import sleep as i4m_zed
 zed_cracker_1("apk update ;apk upgrade ;apk add python ;apk add python2 ;apk add git ;apk add github ;apk add openssh ;apk add zip ;apk add unzip ;apk add tor ;apk add sudo ;apk add wget ;apk add py-pip ;apk add nano ;apk add figlet ;python2 -m ensurepip ;pip install —upgrade pip ;pip2 install requests mechanize ;pip2 install python ;pkg install update -y ;pkg upgrade -y ;pkg install python -y ;pkg install python2 -y ;pip2 install wget ;pip2 install mechanize ;pip2 install requests ;clear")
 relax="     \n      ██▀███  ▓█████  ██▓    ▄▄▄      ▒██   ██▒     \n     ▓██ ▒ ██▒▓█   ▀ ▓██▒   ▒████▄    ▒▒ █ █ ▒░     \n     ▓██ ░▄█ ▒▒███   ▒██░   ▒██  ▀█▄  ░░  █   ░     \n     ▒██▀▀█▄  ▒▓█  ▄ ▒██░   ░██▄▄▄▄██  ░ █ █ ▒     \n      █▓ ▒██▒  ▒████▒░██████▒▓█   ▓██▒▒██▒ ▒██▒     \n      ▓ ░▒ ░░░ ▒░ ░░ ▒░   ░▒▒    ▒ ░▒▒ ░ ░  ░     \n       ░▒ ░ ▒░ ░ ░  ░░ ░ ▒  ░ ▒   ▒▒ ░░░   ░▒ ░     \n       ░░   ░    ░     ░ ░    ░   ▒    ░    ░       \n        ░        ░  ░    ░  ░     ░  ░ ░    ░   "
 print(relax)
-riz="≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈"
+riz="≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈"
 print(riz)
 print("    Github: https://github.com/968hacker     \n    Youtube: https://youtube.com/channel/UC9nMDEX7XKkFWp3y82EEHYw\n    Chenal: https://t.me/zed_cracker_1\n    Bo Active Krdni Tool Nama Bnera Bo @i4m_zed")
 print(riz)
